31710afd71a8f441650dba2b747f8701a7801cd3
Muhammad Lutfi Fahreza <mlutfifahreza@gmail.com>
2023-11-13 23:06:59 +0700

    Time: 35 ms (77.19%), Space: 16.3 MB (53.52%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,22 +1,39 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
+        # stack = []
+        # for ch in s:
+        #     # print(stack)
+        #     try:
+        #         if ch == ")":
+        #             if stack[-1] == "(": stack.pop()
+        #             else: return False
+        #         elif ch == "}":
+        #             if stack[-1] == "{": stack.pop()
+        #             else: return False
+        #         elif ch == "]":
+        #             if stack[-1] == "[": stack.pop()
+        #             else: return False
+        #         else:
+        #             stack.append(ch)
+        #     except:
+        #         return False
+        # if len(stack) > 0:
+        #     return False
+        # return True
+        right = ("}", "]", ")")
+
+        dq = deque()
         for ch in s:
-            # print(stack)
-            try:
-                if ch == ")":
-                    if stack[-1] == "(": stack.pop()
-                    else: return False
-                elif ch == "}":
-                    if stack[-1] == "{": stack.pop()
-                    else: return False
-                elif ch == "]":
-                    if stack[-1] == "[": stack.pop()
-                    else: return False
-                else:
-                    stack.append(ch)
-            except:
-                return False
-        if len(stack) > 0:
-            return False
-        return True
+            if ch in right:
+                if len(dq) == 0:
+                    return False
+                left = dq.pop()
+                if left == "(" and ch is not ")":
+                    return False
+                elif left == "{" and ch is not "}":
+                    return False
+                elif left == "[" and ch is not "]":
+                    return False
+            else:
+                dq.append(ch)
+        return len(dq) == 0
