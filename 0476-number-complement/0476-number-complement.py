fb7d72a47887d4f8509a05cb23fe8220d57cee0b
Muhammad Lutfi Fahreza <mlutfifahreza@gmail.com>
2024-08-22 21:41:52 +0700

    Time: 39 ms (24.08%), Space: 16.6 MB (14.64%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,10 +1,22 @@
 class Solution:
     def findComplement(self, num: int) -> int:
+        # i = 0
+        # res = 0
+        # while num > 0:
+        #     if num%2 == 0:
+        #         res += pow(2, i)
+        #     num //= 2
+        #     i += 1
+        # return res
+
         res = 0
-        i = 0
-        while num > 0:
-            if num & 1 == 0:
-                res |= (1 << i)
-            num >>= 1
-            i += 1
+        for i in range(40):
+            if num < 1:
+                break
+            
+            if num % 2 == 0:
+                res += pow(2, i)
+            
+            num //= 2
+        
         return res
