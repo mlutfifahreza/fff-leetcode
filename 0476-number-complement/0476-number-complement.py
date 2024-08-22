bd65d7b6c3a8045e5c6d92a6bca0ed500d115b11
Muhammad Lutfi Fahreza <mlutfifahreza@gmail.com>
2024-08-22 21:41:16 +0700

    Time: 36 ms (45.66%), Space: 16.6 MB (14.64%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,11 +1,10 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        i = 0
         res = 0
+        i = 0
         while num > 0:
-            if num%2 != 1:
-                # print("pow(2, i)", pow(2, i))
-                res += pow(2, i)
-            num //= 2
+            if num & 1 == 0:
+                res |= (1 << i)
+            num >>= 1
             i += 1
         return res
