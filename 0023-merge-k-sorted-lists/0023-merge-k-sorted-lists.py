63cec01f585a4a4c220c236a3073e921713bf0a4
Muhammad Lutfi Fahreza <mlutfifahreza@gmail.com>
2024-08-26 21:40:10 +0700

    Time: 72 ms (82.96%), Space: 19.4 MB (51.2%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,25 +5,49 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        import heapq
+        if len(lists) == 0:
+            return None
 
-        nums = []
-        for ll in lists:
-            cur = ll
-            while cur:
-                nums.append(cur.val)
-                cur = cur.next
-        # print("nums", nums)
+        def merge_two_list(list_1, list_2):
+            if not list_1:
+                return list_2
+            if not list_2:
+                return list_1
+                
+            dummy = ListNode()
+            curr = dummy
+            while list_1 and list_2:
+                if list_1.val < list_2.val:
+                    curr.next = list_1
+                    list_1 = list_1.next
+                else:
+                    curr.next = list_2
+                    list_2 = list_2.next
+                curr = curr.next
+            
+            while list_1:
+                curr.next = list_1
+                list_1 = list_1.next
+                curr = curr.next
 
-        heapq.heapify(nums)
-        # print("nums", nums)
+            while list_2:
+                curr.next = list_2
+                list_2 = list_2.next
+                curr = curr.next
+            
+            return dummy.next
 
-        dummy = ListNode()
-        cur = dummy
-        while nums:
-            v = heapq.heappop(nums)
-            node = ListNode(v)
-            cur.next = node
-            cur = node
+        while len(lists) > 1:
+            merge_lists = []
+            for i in range(len(lists)):
+                if i % 2 == 1:
+                    res = merge_two_list(lists[i-1], lists[i])
+                    merge_lists.append(res)
+            
+            if len(lists) % 2 == 1:
+                merge_lists.append(lists[-1])
+            
+            lists = merge_lists
 
-        return dummy.next
+        
+        return lists[0]
