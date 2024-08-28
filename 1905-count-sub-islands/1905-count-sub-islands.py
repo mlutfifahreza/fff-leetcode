f77fefe8f343e7908cefdbeaa583af5810565a94
Muhammad Lutfi Fahreza <mlutfifahreza@gmail.com>
2024-08-28 20:43:09 +0700

    Time: 1994 ms (82.01%), Space: 54.5 MB (33.03%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -3,38 +3,28 @@ class Solution:
         self.m, self.n = len(grid1), len(grid1[0])
         self.grid1 = grid1
         self.grid2 = grid2
-        q = []
-        self.visited = []
-        for _ in range(self.m):
-            self.visited.append([False] * self.n)
+
+        def is_island_valid(i, j):
+            if not (0 <= i < self.m and 0 <= j < self.n) or grid2[i][j] == 0:
+                return True
+            if grid1[i][j] == 0:
+                return False
+            
+            # Mark as visited
+            grid2[i][j] = 0
+            
+            # Recursively check all neighbors
+            valid = True
+            for x, y in [(i-1, j), (i+1, j), (i, j-1), (i, j+1)]:
+                if not is_island_valid(x, y):
+                    valid = False
+            
+            return valid
 
         res = 0
         for i in range(self.m):
             for j in range(self.n):
-                if grid2[i][j] == 1 and not self.visited[i][j]:
-                    if self.is_island_valid(i,j):
-                        res += 1
-        
-        # [print(v) for v in self.visited]
-        
-        return res
-    
-    def is_island_valid(self, i, j):
-        if not (0 <= i < self.m):
-            return True
-        if not (0 <= j < self.n):
-            return True
+                if grid2[i][j] == 1 and is_island_valid(i, j):
+                    res += 1
 
-        if self.visited[i][j]:
-            return True
-        
-        if self.grid2[i][j] == 0:
-            return True
-        
-        if self.grid1[i][j] == 0:
-            return False
-        else:
-            self.visited[i][j] = True
-            top, right = self.is_island_valid(i-1,j), self.is_island_valid(i,j+1)
-            down, left = self.is_island_valid(i+1,j), self.is_island_valid(i,j-1)
-            return top and right and down and left
+        return res
