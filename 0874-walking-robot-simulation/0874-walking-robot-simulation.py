7f385e808dc7f57aff916de768548bb09e499a6b
Muhammad Lutfi Fahreza <mlutfifahreza@gmail.com>
2024-09-04 21:09:42 +0700

    Time: 297 ms (29.35%), Space: 23.3 MB (80.18%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.py
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.py
@@ -1,33 +1,27 @@
 class Solution:
     def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
         directions = [
-            [0, 1], # north
-            [1, 0], # east
-            [0, -1], # south
-            [-1, 0], # west
+            (0, 1),   # north
+            (1, 0),   # east
+            (0, -1),  # south
+            (-1, 0),  # west
         ]
 
         x, y, d, res = 0, 0, 0, 0
 
-        obstacles = set([f'{x}.{y}' for x,y in obstacles])
+        obstacles_set = set(map(tuple, obstacles))
 
         for c in commands:
-            # print(f"c={c}")
             if c == -2:
-                d = (d-1) % 4
-                # print(f"d={d} dir={directions[d]}")
+                d = (d - 1) % 4
             elif c == -1:
-                d = (d+1) % 4
-                # print(f"d={d} dir={directions[d]}")
+                d = (d + 1) % 4
             else:
-                while c:
-                    xn, yn =  x + directions[d][0] , y + directions[d][1]
-                    if f'{xn}.{yn}' in obstacles:
-                        c = 0
-                    else:
-                        c -= 1
-                        x,y = xn, yn
-                        res = max(res, x*x + y*y)
-                        # print(f"x={x} y={y} c={c} res={res} ")
+                for _ in range(c):
+                    xn, yn = x + directions[d][0], y + directions[d][1]
+                    if (xn, yn) in obstacles_set:
+                        break
+                    x, y = xn, yn
+                    res = max(res, x * x + y * y)
         
         return res
