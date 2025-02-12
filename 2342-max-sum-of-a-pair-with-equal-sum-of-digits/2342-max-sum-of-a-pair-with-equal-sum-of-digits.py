27ad97b9d17c97c6e6502576fb6d56584e9e9e0c
Muhammad Lutfi Fahreza <mlutfifahreza@gmail.com>
2025-02-12 23:16:46 +0700

    Time: 288 ms (82.6%), Space: 33.7 MB (33.47%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -7,18 +7,18 @@ class Solution:
         return int(res)
 
     def maximumSum(self, nums: List[int]) -> int:
-        memo = {} # sum_digit -> max_num
+        max_of_sum_digit = {} # sum_digit -> max_num
         result = -1
         for n in nums:
             # print("n = ", n)
             sum_digit = self.sum_digit(n)
-            max_before = memo.get(sum_digit, None)
+            max_before = max_of_sum_digit.get(sum_digit, None)
             if max_before:
                 result = max(result, max_before + n)
                 if n > max_before:
-                    memo[sum_digit] = n
+                    max_of_sum_digit[sum_digit] = n
             else:
-                memo[sum_digit] = n
-            # print(f'sum_digit = {sum_digit} max_before = {max_before} memo = {memo} ')
+                max_of_sum_digit[sum_digit] = n
+            # print(f'sum_digit = {sum_digit} max_before = {max_before} max_of_sum_digit = {max_of_sum_digit} ')
         
         return result
