fdf0abfeaaf173336fba859faa1bdfc3158e6a28
Muhammad Lutfi Fahreza <mlutfifahreza@gmail.com>
2025-02-17 16:34:50 +0700

    Time: 55 ms (29.27%), Space: 27 MB (6.91%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        final_set = set()
+        
+        def recur(c, cur, rest):
+            # print()
+            # print(f'recur({c}, {cur}, {rest})')
+            word = cur + c
+            final_set.add(word)
+            # print(f"word = {word} final_set = {final_set}")
+
+            if not rest : return
+
+            for i,v in enumerate(rest):
+                recur(rest[i], word, rest[0:i] + rest[i+1:len(rest)])
+        
+        recur("", "", tiles)
+
+        return len(final_set) - 1
