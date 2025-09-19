5a6a7fb97357095b5b769ca531228c87a1831d6b
Muhammad Lutfi Fahreza <mlutfifahreza@gmail.com>
2025-09-19 07:25:53 +0700

    Time: 123 ms (58.8%), Space: 23.5 MB (91.13%) - LeetHub

--- 3484-design-spreadsheet/3484-design-spreadsheet.py
+++ 3484-design-spreadsheet/3484-design-spreadsheet.py
@@ -5,26 +5,20 @@ class Spreadsheet:
 
     def setCell(self, cell: str, value: int) -> None:
         self.cells[cell] = value
-        # print("setCell", self.cells)
         
     def resetCell(self, cell: str) -> None:
         if cell in self.cells:
             del self.cells[cell]
 
     def getCellValue(self, cell_or_int):
-        # print("getCellValue", cell_or_int)
-        # print("getCellValue", self.cells)
         x = 0
         try:
             x = int(cell_or_int)
         except:
             x = self.cells.get(cell_or_int, 0)
-        # print("getCellValue -> ", x)
         return x
 
-
     def getValue(self, formula: str) -> int:
-        # print("getValue", formula)
         formula = formula[1:]
         a,b = formula.split("+")
         return self.getCellValue(a) + self.getCellValue(b)
