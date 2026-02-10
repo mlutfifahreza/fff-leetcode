a16a644c3db22eb44269704e1828455ea9ad9fc0
Muhammad Lutfi Fahreza <mlutfifahreza@gmail.com>
2026-02-10 19:48:39 +0700

    Time: 1294 ms (58.3%), Space: 19.5 MB (57.02%) - LeetHub

---
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestBalanced(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans = 0
+
+        for i in range(n):
+            evenSet = set()
+            oddSet = set()
+
+            for j in range(i, n):
+                if nums[j] % 2 == 0:
+                    evenSet.add(nums[j])
+                else:
+                    oddSet.add(nums[j])
+
+                if len(evenSet) == len(oddSet):
+                    ans = max(ans, j - i + 1)
+
+        return ans
