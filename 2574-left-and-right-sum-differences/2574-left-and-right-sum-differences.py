165e0e1653a6cb9e453a1799b45fbd61016dc752
Muhammad Lutfi Fahreza <mlutfifahreza@gmail.com>
2026-06-06 14:36:13 +0700

    Time: 4 ms (48.69%), Space: 19.5 MB (57.43%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
@@ -1,23 +1,16 @@
 class Solution:
     def leftRightDifference(self, nums: List[int]) -> List[int]:
-        lsum = []
-        lc = 0
-        rsum = []
-        rc = 0
-        max_idx = len(nums) - 1
-
-        for i,n in enumerate(nums):
-            # print('process', i,n)
-            lsum.append(lc)
-            lc += n
-            rsum.insert(0, rc)
-            rc += nums[max_idx-i]
+        left_sum = 0
+        right_sum = sum(nums)
+        ans = []
         
-        # print(lsum, rsum)
-
-        for i,n in enumerate(lsum):
-            lsum[i] = abs(n - rsum[i])
-        
-        # print(lsum)
-
-        return lsum
+        for num in nums:
+            # Right sum for the current element excludes the element itself
+            right_sum -= num 
+            
+            ans.append(abs(left_sum - right_sum))
+            
+            # Left sum for the NEXT element includes the current element
+            left_sum += num
+            
+        return ans
